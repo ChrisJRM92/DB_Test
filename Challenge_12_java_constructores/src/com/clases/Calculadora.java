d06037d0456bea579b80f0fddb856f9babd733fb
Christian R <cjromerom28@gmail.com>
2025-11-19 20:21:18 -0500

    Reto 12 completado

---
+++ Challenge_12_java_constructores/src/com/clases/Calculadora.java
@@ -0,0 +1,68 @@
+package com.clases;
+public class Calculadora {
+    private double valor1;
+    private double valor2;
+
+    private double dividendo;
+    private double divisor;
+
+    private double valorP1;
+    private double valorP2;
+    private double valorP3;
+
+    public Calculadora(double valor1, double valor2, double dividendo, double divisor, double valorP1, double valorP2, double valorP3){
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+        this.dividendo = dividendo;
+        this.divisor = divisor;
+        this.valorP1 = valorP1;
+        this.valorP2 = valorP2;
+        this.valorP3 = valorP3;
+    }
+
+    public void setValor1(double valor1) {
+        this.valor1 = valor1;
+    }
+
+    public void setValor2(double valor2) {
+        this.valor2 = valor2;
+    }
+
+    public void setDividendo(double dividendo) {
+        this.dividendo = dividendo;
+    }
+
+    public void setDivisor(double divisor) {
+        this.divisor = divisor;
+    }
+
+    public void setValorP1(double valorP1) {
+        this.valorP1 = valorP1;
+    }
+
+    public void setValorP2(double valorP2) {
+        this.valorP2 = valorP2;
+    }
+
+    public void setValorP3(double valorP3) {
+        this.valorP3 = valorP3;
+    }
+
+    public double multiplicar (){
+        return this.valor1*this.valor2;
+    }
+    public double dividir (){
+        return this.dividendo/this.divisor;
+    }
+    public double promediar (){
+        return (this.valorP1+this.valorP2+this.valorP3)/3;
+    }
+
+    public static void mostrarResultado() {
+        System.out.println("Ahorita no joven");
+    }
+
+
+
+
+}
---
+++ Challenge_12_java_constructores/src/com/clases/Producto.java
@@ -0,0 +1,46 @@
+package com.clases;
+public class Producto {
+    private String nombre;
+    private String descripcion;
+    private float precio;
+    private int stockActual;
+
+    public Producto(String nombre, String descripcion, float precio, int stockActual){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stockActual = stockActual;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getStockActual() {
+        return stockActual;
+    }
+
+    public void setStockActual(int stockActual) {
+        this.stockActual = stockActual;
+    }
+}
